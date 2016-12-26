11ffe2587a02bbdcf0bcc51c5e6d3a5dcdefb6df
Bradley Axen <bradley.axen@gmail.com>
2016-12-26 07:38:40 -0800

    Fixed topic in setup.py to add new release to pypi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='fishbowl',
       license='MIT',
       classifiers=[
           'Development Status :: 3 - Alpha',
-          'Topic :: Plotting :: Style',
+          'Topic :: Scientific/Engineering :: Visualization',
           # Pick your license as you wish (should match "license" above)
           'License :: OSI Approved :: MIT License',
           # Specify the Python versions you support here. In particular, ensure
