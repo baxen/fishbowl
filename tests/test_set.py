98bded02b1b5116db640f5e58f73920108af0b0c
Bradley Axen <bradley.axen@gmail.com>
2017-08-01 08:06:47 -0700

    Update tests to remove pgf.preamble

--- tests/test_set.py
+++ tests/test_set.py
@@ -2,31 +2,30 @@ import matplotlib
 import fishbowl
 
 
-original = []
-updated = [r'\usepackage{mathspec}',
-           r'\setallmainfonts(Digits,Latin,Greek){Arbitrary}']
+original = True
+updated = False
 
 
 def test_context_set():
     fishbowl.reset_style()
     with fishbowl.style(axes='minimal', palette='gourami', font='Arbitrary'):
-        assert matplotlib.rcParams['pgf.preamble'] == updated
+        assert matplotlib.rcParams['axes.spines.left'] == updated
 
 
 def test_context_reset():
     fishbowl.reset_style()
     with fishbowl.style(axes='minimal', palette='gourami', font='Arbitrary'):
         pass
-    assert matplotlib.rcParams['pgf.preamble'] == original
+    assert matplotlib.rcParams['axes.spines.left'] == original
 
 
 def test_set():
     fishbowl.reset_style()
     fishbowl.set_style(font='Arbitrary')
-    assert matplotlib.rcParams['pgf.preamble'] == updated
+    assert matplotlib.rcParams['axes.spines.left'] == updated
 
 
 def test_reset():
     fishbowl.set_style(font='Arbitrary')
     fishbowl.reset_style()
-    assert matplotlib.rcParams['pgf.preamble'] == original
+    assert matplotlib.rcParams['axes.spines.left'] == original
